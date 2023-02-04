bce3d3e5170d8dfbd4e796528d7eddb8d17ed06e
AndrewHolmes060 <40767471+AndrewHolmes060@users.noreply.github.com>
2023-02-04 00:07:20 +0000

    Update PlexSync.py
    Updated Sync to use only token authentication and rsync for dynamic IP's
    
    removed watchlist syncing as this is now a function in plex.
    
    fixed folder fixed variable issue.
    
    reduced variables and hard fixed issues.

--- PlexSync.py
+++ PlexSync.py
@@ -1,36 +1,57 @@
 #!/usr/bin/python3
-import plexapi
-from plexapi.myplex import MyPlexAccount
-from plexapi.server import PlexServer
 import pathlib
 import shutil
 import subprocess
+import plexapi
+import arrapi
+from logging import exception
+from plexapi.myplex import MyPlexAccount
+from plexapi.server import PlexServer
+from plexapi.base import PlexObject
+from arrapi import SonarrAPI
 
-#details for home_ server to send files and read watchlist
-ip = 'remote access IP for home_ plex'
-port = 'port of remote access plex'
-home_token = 'token'
-home_tv_folder = '/TVShows/'
-home_movie_folder = '/Movies/'
-user = 'name of rsync user'
+#details for home server to send files and read watchlist
 
+#username for rsync recieiving user
 
-#remote server details to grab videos and sync
-remote_url = 'remote access address for remote plex server'
-token = 'token'
-tv_folder = '/mnt/data/media/TV/'
-movie_folder = '/mnt/data/media/Movies/'
+user = 'user on the recievers rsync machine'
 
-#connect to each server
-home_url = 'http://{}:{}'.format(ip, port)
+#details for recieving server to send files and read watchlist
+home_token = 'token of user whos recieving server'
+home_tv_folder = '/TV/Folder/from/rsync/location/'
+home_movie_folder = '/Movie/Folder/from/rsync/location/'
+server_name = 'Name Of Plex Server to recieve files'
+
+#details for the sending server
+remote_token = 'token of user who managed sending server'
+tv_folder = '/TV/Folder/from/plex/location/'
+movie_folder = '/movie/Folder/from/plex/location/'
+remote_server_name='sending server name'
+
+#Sonarr connect
+# Set Host URL and API-Key
+sonarr_url = 'URL for Sonarr'
+sonarr_api_key = 'Sonarr API Key'
+sonarr = SonarrAPI(sonarr_url, sonarr_api_key)
+
+#connect to Connect To reciever server
 home_account = MyPlexAccount(home_token)
-home_plex = PlexServer(home_url, home_token)
+home_resources = home_account.resources()
+print(home_account)
+home_plex = home_account.resource(server_name).connect()
+connected_server = home_plex.account()
+home_ip = (connected_server.publicAddress)
 
-account = MyPlexAccount(token)
-plex = PlexServer(remote_url, token)
+#connect to sending server
+remote_account = MyPlexAccount(remote_token)
+remote_resources = remote_account.resources()
+remote_plex = remote_account.resource(remote_server_name).connect()
+remote_connected_server = remote_plex.account()
+remote_url = ("http://"+remote_connected_server.publicAddress+":"+remote_connected_server.publicPort)
 
-remote_server_user = home_account.resource('Notflix')
-watched_remote = remote_server_user.connect()
+#connect to remote server
+plex = PlexServer(remote_url, remote_token)
+print(remote_url)
 
 #get user watchlist
 watchlist = home_account.watchlist()
@@ -40,24 +61,40 @@ while item < len(watchlist):
     #search through the watchlist to check for new files
     Watchlist_item = (watchlist[item])
     result = plex.library.search(guid=Watchlist_item.guid, libtype=Watchlist_item.type)
-    home_result = home_plex.library.search(guid=Watchlist_item.guid, libtype=Watchlist_item.type)
     #if the watchlist item is a TV Show
     if Watchlist_item.type == "show" :
         if result != None:
             title = (Watchlist_item.title)
             try:
                 #get file location of tv folder to sync
-                last_episode = plex.library.section('TV').get('{}'.format(title)).episodes()[-1]
+                last_episode = plex.library.section('TV shows').get('{}'.format(title)).episodes()[-1]
                 for part in last_episode.iterParts():
                     directory = (part.file)
                     directory = pathlib.Path(directory)
-                    directory = directory.parts[2]
+                    dir_length = ("{}".format(directory))
+                    dir_list = dir_length.split('/')
+                    directory = directory.parts[len(dir_list)-3]
+                    print(directory)
                     directory = tv_folder+directory
                     print("syncing {}...".format(title))
-                    subprocess.call(["rsync", "-a","-v","--backup","--progress","{}".format(directory), "{}@{}::files{}".format(user, ip, home_tv_folder)])            
-            except Exception:
-                pass
-            #if any errors
+                    subprocess.call(["rsync", "-a","-v","-O","--backup","--progress","{}".format(directory), "{}@{}::files{}".format(user, home_ip, home_tv_folder)])
+                    sonarr_series = sonarr.search_series(title)
+                    sonarr_status = sonarr_series[0].status
+                    print("This show status is: {}".format(sonarr_status))
+                    if sonarr_status == "ended":
+                        #get the list of episodes on the remote server
+                        remote_episodes = set(plex.library.section('TV shows').get('{}'.format(title)).episodes())
+                        remote_formatted_episodes = ["<{}-{}-{}>".format(title, episode.seasonNumber, episode.index) for episode in remote_episodes]
+                        remote_formatted_episodes = set(remote_formatted_episodes)
+                        #get the list of episodes on the home server
+                        home_episodes = set(home_plex.library.section('TV shows').get('{}'.format(title)).episodes())
+                        home_formatted_episodes = ["<{}-{}-{}>".format(title, episode.seasonNumber, episode.index) for episode in home_episodes]
+                        home_formatted_episodes = set(home_formatted_episodes)
+                        #check if the remote server's episodes are a subset of the home server's episodes
+                        if remote_formatted_episodes.issubset(home_formatted_episodes):
+                            #if they are a subset, remove the episode from the watchlist
+                            home_account.removeFromWatchlist(Watchlist_item)
+            except Exception as e: print(e)
             item = item + 1
         else :
             #if  watchlist item isnt on the remote server
@@ -66,19 +103,22 @@ while item < len(watchlist):
 
     #if the watchlist item is a movie
     elif Watchlist_item.type == "movie":
-        if result != None and Watchlist_item:
+        if result != None:
             title = (Watchlist_item.title)
+            print(title)
             try:
                 movie = plex.library.section('Movies').get('{}'.format(title)).media[0].parts[0].file
                 directory = pathlib.Path(movie)
-                directory = directory.parts[2]
+                dir_length = ("{}".format(directory))
+                dir_list = dir_length.split('/')
+                directory = directory.parts[len(dir_list)-2]
+                print(directory)
                 directory = movie_folder+directory
                 print("syncing {}...".format(title))
-                subprocess.call(["rsync", "-a","-v","--backup","--progress","{}".format(directory), "{}@{}::files{}".format(user, ip, home_movie_folder)])
+                subprocess.run(["rsync", "-a","-v","-O","--bwlimit=100000","--backup","--progress","{}".format(directory), "{}@{}::files{}".format(user, home_ip, home_movie_folder)])
                 home_account.removeFromWatchlist(Watchlist_item)
-            except Exception:
-                pass
-            #if any errors
+            except Exception as e: print(e)
+           #if any errors
             item = item + 1
         else :
             #if  watchlist item isnt on the remote server
@@ -86,44 +126,7 @@ while item < len(watchlist):
             print("{} has not arrived on the server yet...".format(title))
     else:
         #finished checking the watchlist
-        item = item + 1
-
-        #syncing watched TV Shows between servers
-        remote_plex_shows = set(list(map((lambda x: x.title), watched_remote.library.section('TV').search())))
-        home_plex_shows = set(list(map((lambda x: x.title), home_plex.library.section('TV Shows').search())))
-        common_shows = remote_plex_shows & home_plex_shows
-        for show_name in common_shows:
-            remote_plex_show = watched_remote.library.section('TV').get(show_name)
-            home_plex_show = home_plex.library.section('TV Shows').get(show_name)
-            for ep in remote_plex_show.episodes():
-                for ep2 in home_plex_show.episodes():
-                    if ep.title == ep2.title:
-                        if ep2.isWatched and not ep.isWatched:
-                            print("marking {} as watched on remote server".format(ep))
-                            ep.markWatched()
-                        elif ep.isWatched and not ep2.isWatched:
-                            print("marking {} as watched on home server".format(ep))
-                            ep2.markWatched()
-                        break
         
-        #syncing watched movies between servers
-        remote_plex_movies = set(list(map((lambda x: x.title), watched_remote.library.section('Movies').search())))
-        home_plex_movies = set(list(map((lambda x: x.title), home_plex.library.section('Films').search())))
-        common_movies = remote_plex_movies & home_plex_movies
-        for movie_name in common_movies:
-            remote_plex_movies = watched_remote.library.section('Movies').get(movie_name)
-            home_plex_movies = home_plex.library.section('Films').get(movie_name)
-            for watched_movie_home in remote_plex_movies:
-                for watched_movie_remote in home_plex_movies:
-                    if watched_movie_home.title == watched_movie_remote.title:
-                        if watched_movie_home.isWatched and not watched_movie_remote.isWatched:
-                            print("marking {} as watched on home server".format(watched_movie_remote))
-                            watched_movie_remote.markWatched()
-                        elif watched_movie_remote.isWatched and not watched_movie_home.isWatched:
-                            print("marking {} as watched on remote server".format(watched_movie_home))
-                            watched_movie_home.markWatched()
-                        break
+        item = item + 1
         print("all done!")
         exit
-
-
